06e88acf61336d75ff6593870dcbe318729b2bc2
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-07-27 16:43:54 +0300

    added JavaScript form Validation

---
+++ sign-up-form/script.js
@@ -0,0 +1,17 @@
+
+//function to validate if the password is a match to the password-confirmation
+function checkPassword(){
+    const userPassword = document.getElementById("user-password").value
+    const passwordConfirmation = document.getElementById("password-confirmation").value
+    const validationNo = document.querySelector(".validation-no");
+    const validationYes = document.querySelector(".validation-yes");
+
+    if (userPassword != passwordConfirmation){
+        validationNo.textContent = "Passwords don't match";
+    }
+    else {
+        validationYes.textContentCon = "Passwords match";
+    }
+    
+}
+
