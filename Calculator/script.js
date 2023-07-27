9fbff49f972b994c8f3a7bd20c70ab40fdbba98a
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-07-27 20:24:41 +0300

    created functions for basic math operators

---
+++ Calculator/script.js
@@ -0,0 +1,20 @@
+
+//function for addition
+function add(a,b) {
+    return a + b;
+}
+
+//function for subtraction
+function subtract(a, b) {
+    return a - b;
+}
+
+//function for multiplication
+function multiply(a, b) {
+    return a * b;
+}
+
+//function for division
+function divide(a, b) {
+    return a / b;
+}
