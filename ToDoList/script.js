91596c4b73a030fee368b3fa7cae84fa1565757b
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-08-08 21:29:11 +0300

    Applied responsive principles to the project

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,7 +1,6 @@
 //list to contain all of the toDos added
 let allToDos = [];
 
-
 // function to convert the form inputs to a single todo object
 function toDo(title, description, notes, dueDate, priority) {
     this.title = title;
