d9bdddefad06f750c9fca378cadbd34d0bf4a4cd
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-08-13 10:21:09 +0300

    Updated the allEquals function to be more readable and functional

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -71,7 +71,16 @@ function changePlayer() {
 function checkWinner() {
 
     //function that goes through an array to check if all the values are equal
-    const allEquals = arr => arr.every(val => val === arr[0]);
+    function allEquals(value1, value2, value3) {
+        if (value1 === value2 && value2 === value3) {
+            result = "true";
+        }
+        else {
+            result = "false";
+        }
+
+        return result;
+    }
     
     let G = GameBoard.gameBoardArray;
     
