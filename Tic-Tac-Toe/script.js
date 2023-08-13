481b017192dd01d8cfd01b3c9f8e1368b43478a3
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-08-13 09:37:08 +0300

    Created a checkWinner function to check if there is a winner after every play

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,14 +1,10 @@
+
 let player1 = {
     play: "x",
 }
 
 let player2 = {
     play: "o",
-}
-
-function print() {
-    console.log(player1.play)
-    console.log(player2.play)
 }
 
 //function to take the value of the selection (X OR O)
@@ -29,7 +25,6 @@ function playSelection() {
 
     player1.play = player1_play;
     player2.play = player2_play;
-    print();
 }
 
 let currentPlayer  = player1.play;
@@ -39,12 +34,13 @@ let GameBoard = {
 }
 
 
-//function to render the contents of the gameBoard to the webpage
+//played block event listener to use an anonymous function to call function clickedBlock()
 const blocks = document.querySelectorAll(".block");
 blocks.forEach(block => block.addEventListener("click", function(){
     clickedBlock(block);
 }))
 
+//function clicked block checks if the block is empty and it it is the calls function updateBlock()
 function clickedBlock(block) {
     const blockIndex = block.getAttribute("blockIndex");
     
@@ -53,13 +49,14 @@ function clickedBlock(block) {
     }
 }
 
+//function to update the block to the corresponding play and call the change player function
 function updateBlock(blockIndex, block) {
     GameBoard.gameBoardArray[blockIndex] = currentPlayer;
     block.textContent = currentPlayer;
     changePlayer();
-    console.log(GameBoard.gameBoardArray);
 }
 
+//function to change the player to play next after every play
 function changePlayer() {
     if (currentPlayer === player1.play) {
         currentPlayer = player2.play;
@@ -67,5 +64,47 @@ function changePlayer() {
     else {
         currentPlayer = player1.play;
     }
+}
+
+//function to check if there is a winner after every play
+function checkWinner() {
+
+    //function that goes through an array to check if all the values are equal
+    const allEquals = aar => array.every(val => val === arr[0]);
+    
+    let G = GameBoard;
+    let g = gameBoardArray;
+    
+    //checking horizontal possible wins.
+    if (allEqualsEquals(G.g[0], G.g[1], G.g[2]) === "true") {
+        displayWinner(G.g[0]);
+    }
+    else if (allEqualsEquals(G.g[3], G.g[4], G.g[5]) === "true") {
+        displayWinner(G.g[3]);
+    }
+    else if (allEqualsEquals(G.g[6], G.g[7], G.g[8]) === "true") {
+        displayWinner(G.g[6]);
+    }
+    //checking vertical possible wins.
+    else if (allEqualsEquals(G.g[0], G.g[3], G.g[6]) === "true") {
+        displayWinner(G.g[0]);
+    }
+    else if (allEqualsEquals(G.g[1], G.g[4], G.g[7]) === "true") {
+        displayWinner(G.g[1]);
+    }
+    else if (allEqualsEquals(G.g[2], G.g[5], G.g[8]) === "true") {
+        displayWinner(G.g[2]);
+    }
+    //checking diagonal possible wins.
+    else if (allEqualsEquals(G.g[0], G.g[4], G.g[8]) === "true") {
+        displayWinner(G.g[0]);
+    }
+    else if (allEqualsEquals(G.g[2], G.g[4], G.g[6]) === "true") {
+        displayWinner(G.g[2]);
+    }
+    else {
+        //must display "keep on playing"
+    }
+
 }
 
