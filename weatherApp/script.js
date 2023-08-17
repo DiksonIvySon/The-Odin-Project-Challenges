59a6013d3bfed3def4526b818f513a40901d7cd6
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-08-17 23:37:00 +0300

    Made styling changes to the first page UI

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -3,26 +3,18 @@
 let localTime;
 let last_updated;      //Day and time.
 let temp_c;
-let temp_f;
 let is_day;            //if 1 then it is day.
 let conditionText;     //whether its sunny rainy, or windy.
 let conditionIcon;
-let wind_mph;
 let wind_kph;
-let wind_degree;
 let wind_dir;
 let pressure_mb;
-let pressure_in;
 let precip_mm;
-let precip_in;
 let humidity;
 let cloud;
 let feelslike_c;
-let feelslike_f;
 let vis_km;
-let vis_miles;
 let uv;
-let gust_mph;
 let gust_kph;
 
 
