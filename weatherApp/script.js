aadd6740f1313acd3cd6848fe84e0ca6dfab3a50
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-08-18 18:30:24 +0300

    Defined all the global variables in the dataInitializer function

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,21 +1,39 @@
 
 //Variables of all the information that will be needed from the Weather API.
+//location
 let localTime;
+let country;
+let lat;
+let lon;
+let locationName;
+let region;
+let tz_id;            //e.g Africa/Johannesburg.
+
+//current
 let last_updated;      //Day and time.
+let last_updated_epoch;
 let temp_c;
+let temp_f;
 let is_day;            //if 1 then it is day.
 let conditionText;     //whether its sunny rainy, or windy.
 let conditionIcon;
+let conditionCode ;
 let wind_kph;
 let wind_dir;
+let wind_degree;
+let wind_mph;
 let pressure_mb;
-let precip_mm;
+let precipitation_mm;
+let precipitation_in;
 let humidity;
 let cloud;
-let feelslike_c;
+let feelsLike_c;
+let feelsLike_f;
 let vis_km;
+let vis_miles;
 let uv;
 let gust_kph;
+let gust_mph;
 
 
 //function to get the data from the API and initialize the weather information variables
@@ -23,7 +41,7 @@ async function getWeatherInfo(location) {
     const response = await fetch(location, {mode: 'cors'});
     const weatherData = await response.json();
     dataInitializer(weatherData);
-  }
+}
 
 
 //function to create the URL link.
@@ -45,7 +63,46 @@ searchButton.addEventListener('click', function() {
 })
 
 function dataInitializer(weatherData) {
-    conditionIcon = weatherData.current.condition.icon;
+
+    //location
+    localTime = weatherData.location.localtime;
+    localTime_epoch = weatherData.location.localtime_epoch;
+    country = weatherData.location.country;
+    lat = weatherData.location.lat;
+    lon = weatherData.location.lon;
+    locationName = weatherData.location.name;
+    region = weatherData.location.region;
+    tz_id = weatherData.location.tz_id;
+
+    //current
+     last_updated = weatherData.current.last_updated;     
+     last_updated_epoch = weatherData.current.last_updated_epoch;
+     temp_c = weatherData.current.temp_c;
+     temp_f = weatherData.current.temp_f;
+     is_day = weatherData.current.is_day;           
+     conditionText = weatherData.current.condition.text;     
+     conditionIcon = weatherData.current.condition.icon;
+     conditionCode = weatherData.current.condition.code;
+     wind_kph = weatherData.current.wind_kph;
+     wind_dir = weatherData.current.wind_dir;
+     wind_degree = weatherData.current.wind_degree;
+     wind_mph = weatherData.current.wind_mph;
+     pressure_mb = weatherData.current.pressure_mb;
+     precipitation_mm = weatherData.current.precip_mm;
+     precipitation_in = weatherData.current.precip_in;
+     humidity = weatherData.current.humidity;
+     cloud = weatherData.current.cloud;
+     feelsLike_c = weatherData.current.feelslike_c;
+     feelsLike_f = weatherData.current.feelslike_f;
+     vis_km = weatherData.current.vis_km;
+     vis_miles = weatherData.current.vis_miles;
+     uv = weatherData.current.uv;
+     gust_kph = weatherData.current.gust_kph;
+     gust_mph = weatherData.current.gust_mph;
+
+
+
+    console.log(weatherData); //
     displayData();
 }
 
@@ -53,6 +110,9 @@ function displayData() {
     let conditionIconScreen = document.querySelector('.conditionIcon');
     conditionIconScreen.setAttribute('src', conditionIcon);
 }
+
+
+
 
 
 
