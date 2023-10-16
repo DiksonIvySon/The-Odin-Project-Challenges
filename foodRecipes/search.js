ff387644bca029ce046d051eabfab53c6071002e
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-16 08:15:19 +0300

    Created a function to get the API data

--- foodRecipes/search.js
+++ foodRecipes/search.js
@@ -5,6 +5,13 @@ search_button.addEventListener('click', handleSearch);
 
 //function to handle search button click
 function handleSearch() {
-    let search_input = document.querySelector('#search-input').value;
-    console.log(search_input);
+    let search_input = document.querySelector('#search-input').value.trim();
+    fetchData("https:/www.themealdb.com/api/json/v1/1/filter.php?i=chicken");
 }
+
+async function fetchData(link) {
+    const response = await fetch(link, {mode: 'cors'})
+    const recipeData = await response.json();
+    console.log(recipeData);
+}
+
