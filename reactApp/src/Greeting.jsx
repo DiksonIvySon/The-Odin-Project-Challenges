6ac0aa639b5960e1697262ec622631ebf62bf280
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2025-02-02 11:05:32 +0200

    renamed files.

---
+++ reactApp/src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greeting() {
+  return (
+    <div className='greeting'>
+        <h1>&quot;Hey there&quot;</h1>
+    </div>
+  )
+}
+
+export default Greeting;
