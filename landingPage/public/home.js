4fee6e1173f5bdc11e43e5d869629536d2e22821
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2025-06-18 16:10:04 +0200

    Updated the tours layout.

--- landingPage/public/home.js
+++ landingPage/public/home.js
@@ -12,7 +12,7 @@ async function fetchEvents() {
 
         const data = await response.json();
         const events = data._embedded.events; 
-        console.log(data.products);
+        console.log(data);
 
         displayEvents(events);
     } catch (error) {
