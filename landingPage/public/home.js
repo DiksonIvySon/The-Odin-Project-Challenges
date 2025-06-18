7d2e4534bc53f96bd9651a427a08b6a390cf6048
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2025-06-18 20:20:57 +0200

    Made the trending events section functional.

--- landingPage/public/home.js
+++ landingPage/public/home.js
@@ -77,6 +77,65 @@ function displayEvents(events) {
     });
 }
 
+// Display the New York City Events
+
+async function searchCityEvents() {
+    let category = "music";
+    let location = "New York City";
+
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=${category}&city=${location}&apikey=${API_KEY}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        if (data._embedded && data._embedded.events) {
+            displayCityEvents(data._embedded.events);
+        } else {
+            document.getElementById("cityEventNotFound").innerHTML = `<h1 class="notFound">No events found.</h1>`;
+        }
+    } catch (error) {
+        console.error("Error fetching events:", error);
+    }
+}
+
+function displayCityEvents(events) {
+    document.getElementById("cityEventNotFound").innerHTML = ""; 
+    let container = document.getElementById("cityEvents-container");
+    container.innerHTML = ""; 
+    console.log(events)
+
+    events.forEach(event => {
+        let eventDiv = document.createElement("div");
+        eventDiv.setAttribute("class", "tour-row");
+
+        eventDiv.innerHTML = `
+            <div class="event-item">
+                <img src="${event.images && event.images.length > 0 ? event.images[0].url : 'placeholder.jpg'}" alt="${event.name}">
+            </div>
+
+            <div class="event-item-details">
+                <span class="tour-item tour-city"> <span class="city-event-name">${event.name}</span> <br> ${new Date(event.dates.start.localDate).toLocaleDateString()} <br> ${event.dates.start.localTime.slice(0,5)}</span>
+            </div>
+            
+            <div>
+                <span class="tour-item tour-arena">${event._embedded.venues[0].name}, ${event._embedded.venues[0].city.name}</span>
+            </div>
+            
+            
+            <a href="${event.url}" target="_blank">
+                <button type="button" class="btn tour-item tour-btn btn-primary">BUY TICKETS</button>
+            </a>
+        `;
+
+        container.appendChild(eventDiv);
+    });
+}
+
 
 fetchEvents();
+searchCityEvents()
 
